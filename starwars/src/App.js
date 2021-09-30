0318f691fbd7a445ce87827de27687b933e9648b
tannerpace <52814471+tannerpace@users.noreply.github.com>
2021-09-29 23:24:11 -0400

    adds browser router

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,51 +1,42 @@
-import React, {useEffect,useState} from "react"
-import RandomNumber from "./functions/RandomNumber"
-import MyForm from "./components/TextInputWithFocusButton"  
-import {Box,Button, Typography} from "@mui/material"
-import Page from "./Pages"
-import Character from "./components/character"
-import quotes from "./functions/quotes"
-import Planet from "./components/planet"
-
-
+import React, { useEffect, useState } from "react";
+import RandomNumber from "./functions/RandomNumber";
+import MyForm from "./components/TextInputWithFocusButton";
+import { AppBar, Box, Button, Typography } from "@mui/material";
+import Page from "./Pages";
+import Character from "./components/character";
+import quotes from "./functions/quotes";
+import Planet from "./components/planet";
 
 // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
 // https://swapi.co/
 // https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
 
 const App = () => {
+  const [page, setPage] = useState("home");
 
+  function swapPages(newPage) {
+    setPage((prevPage) => newPage);
+  }
 
-  const [person,setPerson] = useState({person:'No 1'})
+  const [person, setPerson] = useState({ person: "No 1" });
 
-    // Similar to componentDidMount and componentDidUpdate:
-    const getQuote = ()=>{ 
-      const quote = quotes()
-    return quote}
-   
-    
+  // Similar to componentDidMount and componentDidUpdate:
+  const getQuote = () => {
+    const quote = quotes();
+    return quote;
+  };
 
-return(
+  return (
+    <Page>
+      <Box width="50%" margin="auto">
+        <Typography onCLick={getQuote()}>{`${getQuote()}`}</Typography>
 
-<Page>
+        <AppBar>{/* <MyForm/> */}</AppBar>
+        <Character />
 
-<Box
-width="50%"
-margin="auto"><Typography
- onCLick={getQuote()} 
- >{`${getQuote()}`}
- </Typography>
-
-<Character/>
-<Planet/>
-{/* <MyForm/> */}
-  </Box>
-</Page>)
-}
-export default App
-
-
-
-
-
-
+        <Planet />
+      </Box>
+    </Page>
+  );
+};
+export default App;
